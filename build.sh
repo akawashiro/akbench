e3de964e1ee0005917d2f6210107047d5db5d58e
Akira Kawata <akirakawata1@gmail.com>
2025-07-23 11:19:43 +0900

    Build and Formatting Updates
    - Added a new build script (build.sh) that sets compiler flags and builds the project with Ninja.
    - Added a new script (format.sh) that formats C++ files using clang-format-18 and CMake files using cmake-format.

---
+++ build.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -eux -o pipefail
+
+git submodule update --init --recursive
+cmake -S . \
+    -B build \
+    -D CMAKE_CXX_COMPILER=clang++ \
+    -D CMAKE_CXX_FLAGS="-stdlib=libc++" \
+    -D CMAKE_BUILD_TYPE=RelWithDebInfo \
+    -D CMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -G Ninja
+cmake --build build
---
+++ format.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+set -eux -o pipefail
+
+for file in $(git ls-files | grep -E '\.(cpp|h|hpp|c|cc|cxx)$'); do
+    clang-format-18 -i "$file"
+done
+pipx run cmake-format -i ./CMakeLists.txt
