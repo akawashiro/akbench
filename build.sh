b159dc87dd0c44d0101b43c8820e61f30b4b9566
Akira Kawata <akirakawata1@gmail.com>
2025-07-24 12:17:54 +0900

    Update CMake command

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@ set -eux -o pipefail
 git submodule update --init --recursive
 cmake -S . \
     -B build \
-    -D CMAKE_CXX_COMPILER=clang++ \
-    -D CMAKE_CXX_FLAGS="-stdlib=libc++" \
     -D CMAKE_BUILD_TYPE=RelWithDebInfo \
     -D CMAKE_EXPORT_COMPILE_COMMANDS=ON
 cmake --build build -j
