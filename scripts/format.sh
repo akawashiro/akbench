c0abaddb0996dbe6078db5ce83e1c047c97085b7
Akira Kawata <akirakawata1@gmail.com>
2025-07-26 11:43:46 +0900

    Use cmake_file variable for CMake formatting instead of hardcoded path

--- scripts/format.sh
+++ scripts/format.sh
@@ -9,5 +9,5 @@ for cc_file in $(git ls-files | grep -E '\.(cpp|h|hpp|c|cc|cxx)$'); do
     clang-format-18 -i "$cc_file"
 done
 for cmake_file in $(git ls-files | grep -E 'CMakeLists.txt|.*\.cmake$'); do
-    pipx run cmake-format -i ./CMakeLists.txt
+    pipx run cmake-format -i "$cmake_file"
 done
